15c6b279ff7033ebcb6bdb12e717959d75cabe43
Beatriz R. Aguiar <beatriz.ruiz7@educa.madrid.org>
2025-01-29 19:32:01 +0100

    consigo obtener el tablero, más o menos

--- Tres en Raya/scripRaya.js
+++ Tres en Raya/scripRaya.js
@@ -6,20 +6,11 @@ let casillaJug1;
 let casillaJug2;
 let finalTiempo=false;
 let casillaOcupada=true;
-let arrayTablero=[['-','-','-'],['-','-','-'],['-','-','-']];
+//let arrayTablero=[['-','-','-'],['-','-','-'],['-','-','-']];
 while(finPartida){
-    //Jugador 1:
-
-        //pedir lugar casilla
-        while(casillaOcupada){
-            //pedir lugar casilla
-
-        }
-        console.log()//ficha en el tablero
-        //comprobar ganador
-        if(ganador){break}
-
-    //Jugador 2:
+    for(let i=0;i<2;i++){
+        //Una ronda por jugador:
+        //mostrar qué jugador toca por pantalla
 
         //pedir lugar casilla
         //función para revisar si la casilla está vacía
@@ -34,6 +25,8 @@ while(finPartida){
         //comprobar ganador
         if(ganador){break}
 
+    }
+    
 
 }
 function xTablero(e){
@@ -44,5 +37,22 @@ function xTablero(e){
 function oTablero(e){
     const casilla=document.getElementById(e.toString());
     casilla.innerText='O'
+
+}
+//creo que no necesito esto
+function arrTablero(){
+    let contador=0
+    let arrayTablero=[]
+    for(let i=0;i<3;i++){
+        
+        for(let y=0;y<3;y++){
+            contador++;
+            let casilla=document.getElementById(contador.toString());
+            let interior=casilla.innerText;
+            arrayTablero[i,contador]=interior;
+
+        }
+    }
+    console.log(arrayTablero.join("\n"));
 
 }
